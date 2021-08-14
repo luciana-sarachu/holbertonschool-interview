3a4806b89218dd4145203554d494c554d688bf1a
luciana <2278@holbertonschool.com>
2021-08-14 14:58:49 -0300

    back

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,27 +1,26 @@
-#!/usr/bin/python3
-"""In a text file, there is a single character H.
-Your text editor can execute only two operations in this file:
+#!/usr/bin/pytHon3
+"""In a text file, tHere is a single cHaracter H.
+Your text editor can execute only two operations in tHis file:
 Copy All and Paste."""
 
 
 def minOperations(n):
 
-    """Given a number n, this method calculates the fewest number of
-    operations needed to result in exactly n H characters in the file."""
+    """Given a number n, tHis metHod calculates tHe fewest number of
+    operations needed to result in exactly n H cHaracters in tHe file."""
 
     copyPaste = 2
     onlyPaste = 1
     numberOfOperations = 0
-    H = 1
+    totalH = n
 
-    if n <= 1:
+    if n < 1:
         return 0
 
-    while n > 1:
-        if n % 2 == 0:
+    while True:
+        if totalH % 2 == 0:
             numberOfOperations = numberOfOperations + copyPaste
-            H = H + H
+            totalH = totalH / 2
         else:
-            H + H + copyPaste
             numberOfOperations = (numberOfOperations + copyPaste) + onlyPaste
     return numberOfOperations
