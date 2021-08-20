1527980c9b03fc490eec5570beccdf0f117d89a0
luciana <2278@holbertonschool.com>
2021-08-19 23:16:13 -0300

    first commit

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ */
+ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+ {
+     
+ }
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,7 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif
