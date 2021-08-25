7b9ec39bd48dcee874544ed37aa736c6c0677b06
luciana <2278@holbertonschool.com>
2021-08-25 16:25:09 -0300

    palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -6,13 +6,13 @@
  * Return: 1 or 0
  */
 int is_palindrome(listint_t **head)
-{  
-    listint_t *aux = *head;
+{
+	listint_t *aux = *head;
 	int array[1024], i = 0, ii = 0, middle;
 
 	if (head == NULL)
 		return (1);
-        
+
 	while (aux != NULL)
 	{
 		array[i] = aux->n;
@@ -20,7 +20,7 @@ int is_palindrome(listint_t **head)
 		i++;
 	}
 	i--;
-    middle = i / 2;
+	middle = i / 2;
 	while (i >= middle && ii <= middle)
 	{
 		if (array[ii] != array[i])
