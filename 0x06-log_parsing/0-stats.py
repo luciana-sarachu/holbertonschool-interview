f082d6e51e7d13cded77bc17718889c29180396d
luciana <2278@holbertonschool.com>
2021-08-31 23:56:24 -0300

    task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+
+"""script that reads stdin line by line and computes metrics"""
+
 import sys
 
 status = [200, 301, 400, 401, 403, 404, 405, 500]
 
+nbrOfLines = 0
+size = 0
+
 for line in sys.stdin:
-    print(line)
+    metrics = line.split()
+    size += int(metrics[-1])
+    stat = metrics[-2]
+    if stat in status:
+        print("File size: {}".format(size))
+        nbrOfLines += 1
+        if nbrOfLines == 9 or KeyboardInterrupt:
+            print("File size: {}".format(size))
+    else:
+        pass
