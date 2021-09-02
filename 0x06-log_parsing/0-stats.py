072e373258b0b4c4cb09f292a06fbb95495bc688
luciana <2278@holbertonschool.com>
2021-09-02 01:53:22 -0300

    changes

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,22 +1,28 @@
-#!/usr/bin/python3
+y#!/usr/bin/python3
 
 """script that reads stdin line by line and computes metrics"""
 
 import sys
 
-status = [200, 301, 400, 401, 403, 404, 405, 500]
+status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
 nbrOfLines = 0
 size = 0
 
-for line in sys.stdin:
-    metrics = line.split()
-    size += int(metrics[-1])
-    stat = metrics[-2]
-    if stat in status:
+try: 
+    for line in sys.stdin:
+        try:
+            metrics = line.split()
+            size += int(metrics[-1])
+            stat = metrics[-2]
+            if stat in status:
+                status[stat] += 1
+        except:
+            pass
         print("File size: {}".format(size))
-        nbrOfLines += 1
-        if nbrOfLines == 9 or KeyboardInterrupt:
-            print("File size: {}".format(size))
-    else:
-        pass
+        if nbrOfLines == 9:
+            print("{}: {}".format(stat, status[stat]))
+            nbrOfLines += 1
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+
