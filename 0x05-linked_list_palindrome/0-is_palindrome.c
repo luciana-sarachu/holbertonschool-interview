39736a024f282828bf62ba6f6f983e21cc224e1a
luciana <2278@holbertonschool.com>
2021-09-18 21:55:17 -0300

    fix task

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,27 +2,28 @@
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to head of list
- * Return: 1 or 0
+ * @head: list to check
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *aux = *head;
+	listint_t *aux;
 	int array[1024], i, ii;
 
-	if (*head == NULL)
+	if (*head == 0)
 		return (1);
+
+	aux = *head;
 
 	for (i = 0; aux != NULL; i++)
 	{
 		array[i] = aux->n;
 		aux = aux->next;
 	}
+
 	for (ii = 0; ii < i; ii++)
-	{
-		/* As i increment I have to subtract 1 to stand at the end of the array */
 		if (array[ii] != array[i - ii - 1])
 			return (0);
-	}
+
 	return (1);
 }
--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,8 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * struct listint_s - singly linked list
