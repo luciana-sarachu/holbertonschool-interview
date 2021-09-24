75686c718d83acf2bfd44ec554beb4508fd18ed6
luciana <2278@holbertonschool.com>
2021-09-23 23:43:01 -0300

    first commit

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" https://helloacm.com/how-to-validate-utf-8-encoding-the """
+""" -simple-utf-8-validation-algorithm/ """
+
+
+def validUTF8(data):
+    """ determine if a given data set is a valid UTF-8 encoding """
