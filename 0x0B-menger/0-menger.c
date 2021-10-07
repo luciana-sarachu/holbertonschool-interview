6d678247060889b2c5b5915de5bb4958c95e5503
luciana <2278@holbertonschool.com>
2021-10-06 23:25:27 -0300

    add secondary function

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,28 +1,34 @@
 #include "menger.h"
 /**
+ * choose_hashtag_or_blank - return hashtag or leave empty
+ * @col:  is th column
+ * @row: is the row
+ * Return: '#' or ' '
+ */
+char choose_hashtag_or_blank(int col, int row)
+{
+	for (; col && row; col /= 3, row /= 3)
+		if (col % 3 == 1 && row % 3 == 1)
+			return (' ');
+	return ('#');
+}
+
+/**
  * menger - draws a 2D Menger Sponge
  * @level: is the level of the Menger Sponge to draw
  */
 void menger(int level)
 {
-	/** [Mathematics] xy = pow(x, y) [In programming] **/
 	int col, row, size;
-
 
 	if (level >= 0)
 	{
+		/** [Mathematics] xy = pow(x, y) [In programming] **/
 		size = pow(3, level);
 		for (col = 0; col < size; col++)
 		{
 			for (row = 0; row < size; row++)
-				if (col % 3 == 1 && row % 3 == 1)
-				{
-					printf(" ");
-				}
-				else
-				{
-					printf("#");
-				}
+				printf("%c", choose_hashtag_or_blank(col, row));
 			printf("\n");
 		}
 	}
