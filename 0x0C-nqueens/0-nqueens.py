4e3ecd9ae251de3136a3e8f66f3c4540cdf64ea6
luciana <2278@holbertonschool.com>
2021-10-23 23:35:35 -0300

    task fixed

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -6,21 +6,46 @@ solucion-reto-de-las-8-reinas-en-python """
 from sys import argv, exit
 
 
-if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Usage: nqueens N\n")
-        exit(1)
+if len(argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    N = argv[1]
-    if type(N) is not int:
-        print("N must be a number\n")
-        exit(1)
+N = int(argv[1])
+if type(N) != int:
+    print("N must be a number")
+    exit(1)
 
-    if N <= 4:
-        print("N must be at least 4\n")
-        exit(1)
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    solution = []
+solution = []
 
-    def nqueens(N):
-            """ The program should print every possible solution"""
+
+def nqueens(row, N, solution):
+    """The program should print any possible solution"""
+    if (row == N):
+        print(solution)
+    else:
+        for col in range(N):
+            position = [row, col]
+            if validposition(solution, position):
+                solution.append(position)
+                nqueens(row + 1, N, solution)
+                solution.remove(position)
+
+
+def validposition(solution, position):
+    """validate horizontal and diagonal position of queens"""
+    for queen in solution:
+        if queen[1] == position[1]:
+            return False
+        # descending diagonal
+        if (queen[0] - queen[1]) == (position[0] - position[1]):
+            return False
+        # ascending diagonal
+        if (queen[0] + queen[1]) == (position[0] + position[1]):
+            return False
+    return True
+
+nqueens(0, N, solution)
