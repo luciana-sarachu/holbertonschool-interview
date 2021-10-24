3c53751b6e54adcd1ff6430030a7873e2d6dfe61
luciana <2278@holbertonschool.com>
2021-10-23 23:47:31 -0300

    nqueens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -6,45 +6,46 @@ solucion-reto-de-las-8-reinas-en-python """
 from sys import argv, exit
 
 
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
+if len(argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    N = int(argv[1])
-    if type(N) != int:
-        print("N must be a number")
-        exit(1)
+N = int(argv[1])
+if type(N) != int:
+    print("N must be a number")
+    exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    solution = []
+solution = []
 
-    def nqueens(row, N, solution):
-        """The program should print any possible solution"""
-        if (row == N):
-            print(solution)
-        else:
-            for col in range(N):
-                position = [row, col]
-                if validposition(solution, position):
-                    solution.append(position)
-                    nqueens(row + 1, N, solution)
-                    solution.remove(position)
 
-    def validposition(solution, position):
-        """validate horizontal and diagonal position of queens"""
-        for queen in solution:
-            if queen[1] == position[1]:
-                return False
-            # descending diagonal
-            if (queen[0] - queen[1]) == (position[0] - position[1]):
-                return False
-            # ascending diagonal
-            if (queen[0] + queen[1]) == (position[0] + position[1]):
-                return False
-        return True
+def nqueens(row, N, solution):
+    """The program should print any possible solution"""
+    if (row == N):
+        print(solution)
+    else:
+        for col in range(N):
+            position = [row, col]
+            if validposition(solution, position):
+                solution.append(position)
+                nqueens(row + 1, N, solution)
+                solution.remove(position)
 
-    nqueens(0, N, solution)
+
+def validposition(solution, position):
+    """validate horizontal and diagonal position of queens"""
+    for queen in solution:
+        if queen[1] == position[1]:
+            return False
+        # descending diagonal
+        if (queen[0] - queen[1]) == (position[0] - position[1]):
+            return False
+        # ascending diagonal
+        if (queen[0] + queen[1]) == (position[0] + position[1]):
+            return False
+    return True
+
+nqueens(0, N, solution)
