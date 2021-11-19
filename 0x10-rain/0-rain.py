c4aa0dd6c8d2a568f961512ded0ef47e0cf393f1
luciana <2278@holbertonschool.com>
2021-11-18 23:35:57 -0300

    deleted wrong import'

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -2,7 +2,6 @@
 """Given a list of non-negative integers representing the heights of
 walls with unit width 1, as if viewing the cross-section of a relief map,
 calculate how many square units of water will be retained after it rains."""
-from typing import TextIO
 
 
 def rain(walls):
