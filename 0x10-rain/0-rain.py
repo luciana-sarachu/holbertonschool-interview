a0cc0aecbe72f2c2a968cffbc812c281c81e357d
luciana <2278@holbertonschool.com>
2021-11-18 23:32:02 -0300

    fix shebang

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,4 +1,4 @@
-#!usr/bin/env phyton3
+#!usr/bin/phyton3
 """Given a list of non-negative integers representing the heights of
 walls with unit width 1, as if viewing the cross-section of a relief map,
 calculate how many square units of water will be retained after it rains."""
