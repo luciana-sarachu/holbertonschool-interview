4426f8ecf5c67c0423b457a053924ad4a32b7938
luciana <2278@holbertonschool.com>
2022-01-05 23:47:02 -0300

    first commit

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Given an n x n 2D matrix, rotate it 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """https://stackoverflow.com/questions/51804790/in-place-modification-of-python-lists"""
+    n = len(matrix)
+    
